2c04f5e22c5b94d0db3ee275a45e0e2d04077be8
Nikolay Murzin <nikm@wolfram.com>
2023-09-20 15:40:19 +0300

    make WFR symbol names from UUIDs

--- Multicomputation/Kernel/WFR.m
+++ Multicomputation/Kernel/WFR.m
@@ -2,22 +2,23 @@ $WFR = {
     <|
         "Name" -> "MultiReplace",
         "ResourceType" -> "Function",
-        "FunctionLocation" -> CloudObject["https://www.wolframcloud.com/obj/nikm/Resources/e95/e95e886d-0871-40ac-9167-a03825e05f6b/download/DefinitionData"],
-        "SymbolName" -> "FunctionRepository`$e95e886d087140ac9167a03825e05f6b`MultiReplace"
+        "FunctionLocation" -> "https://www.wolframcloud.com/obj/nikm/Resources/e95/e95e886d-0871-40ac-9167-a03825e05f6b/download/DefinitionData"
     |>,
     <|
         "Name" -> "MultiEvaluate",
         "ResourceType" -> "Function",
-        "FunctionLocation" -> CloudObject["https://www.wolframcloud.com/obj/nikm/Resources/135/13505f23-b475-4250-a08a-1e2d6a9a88ad/download/DefinitionData"],
-        "SymbolName" -> "FunctionRepository`$13505f23b4754250a08a1e2d6a9a88ad`MultiEvaluate"
+        "FunctionLocation" -> "https://www.wolframcloud.com/obj/nikm/Resources/135/13505f23-b475-4250-a08a-1e2d6a9a88ad/download/DefinitionData"
     |>,
     <|
         "Name" -> "TokenEventGraph",
         "ResourceType" -> "Function",
-        "FunctionLocation" -> CloudObject["https://www.wolframcloud.com/obj/cb8a4383-cdcb-4ef0-9b7e-5acd4edbc4e2"],
-        "SymbolName" -> "FunctionRepository`$a01ca9049fdd4b21abf697f2e4a860d8`TokenEventGraph"
+        "FunctionLocation" -> "https://www.wolframcloud.com/obj/cb8a4383-cdcb-4ef0-9b7e-5acd4edbc4e2"
     |>
 }
+
+makeSymbolName[uuid_String, name_String] := "FunctionRepository`$" <> StringDelete[uuid, "-"] <> "`" <> name;
+
+$WFR = Append[#, "SymbolName" -> makeSymbolName[Import[#["FunctionLocation"]]["UUID"], #["Name"]]] & /@ $WFR
 
 Scan[data |->
     With[{sym = Symbol[$Context <> data["Name"]],
